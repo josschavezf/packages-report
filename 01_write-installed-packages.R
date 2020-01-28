13c491806f7447377ce59ac7f7d7c20be62a94c1
Joselyn Chavez <joselynchavezf@gmail.com>
2020-01-27 16:46:51 -0800

    A line for writting your table was added

--- 01_write-installed-packages.R
+++ 01_write-installed-packages.R
@@ -19,3 +19,11 @@ pkgs <- as.data.frame(installed.packages())
 
 pkgs <- dplyr::as_tibble(pkgs %>% select(Package, LibPath, Version, Priority,  Built))
 
+#' Export your data
+
+## write this data frame to data/installed-packages.csv
+## hint: readr::write_csv() or write.table()
+## idea: try using here::here() to create the file path
+
+library(here)
+readr::write_csv(pkgs, path = here::here("data", "installed-packages.csv"))
